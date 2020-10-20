3efbe8d6e1263e64deb2dd9bf7162f8c0c0119a7
xxxx <xr@i-on.net>
2020-10-20 17:31:24 +0900

    VUE NUXT TEST TEMPLATE

---
+++ client/components/listItem.tsx
@@ -0,0 +1,18 @@
+import { CreateElement, VNode } from 'vue'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+
+@Component
+export default class listItem extends Vue {
+  @Prop({ type: Number, required: true, default: 0 }) num!: number;
+
+  render(h: CreateElement): VNode | null{
+    console.log("listItem ==========",this);
+    const { num } = this;
+    return (
+      <li>{num}</li>
+    )
+
+  }
+
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,16 @@
 import { CreateElement, VNode } from 'vue'
 import { Component, Vue } from "nuxt-property-decorator";
 
-@Component
+import ListItem from "@/components/listItem";
+
+
+@Component({
+  components:{
+    ListItem
+  }
+})
 export default class MainPage extends Vue {
+
   async asyncData({ app, store }: nuxtContext): Promise<any>{
     console.log("-------page index -------", app, store);
     let parallel = store.dispatch('operation/arrayHandler', store);
@@ -11,13 +19,22 @@ export default class MainPage extends Vue {
     await parallel1;
   }
 
+  private listLength: number[] = [1,2,3,4,5];
+
   mounted(): void{
     console.log("pages/index.tsx");
   }
 
   render(h: CreateElement): VNode{
     return (
-      <div><p>Main</p></div>
+      <div>
+        <p>Main</p>
+        <ul>
+          { this.listLength.map(item => (
+            <ListItem num={ item }  />
+          )) }
+        </ul>
+      </div>
     )
   }
 
